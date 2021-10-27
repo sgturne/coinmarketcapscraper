4a61df55cd27689b12894874439bd4a25a5e5e5d
Sarah Turner <sgturne@Sarahs-MBP-2.attlocal.net>
2021-10-26 22:11:57 -0400

    download multiple pages

---
+++ coinmarketcap_request_mulitpage.py
@@ -0,0 +1,28 @@
+
+import urllib.request
+import os
+
+import datetime
+import time
+
+if not os.path.exists("html_files"):
+	os.mkdir("html_files")
+
+
+
+for i in range(1):
+	current_time_stamp = datetime.datetime.fromtimestamp(time.time()).strftime("%Y%m%d%H%M%S")
+	print(current_time_stamp)
+
+	for j in range(5):
+		page_num=j+1
+		f = open("html_files/coinmarketcap" + "_" + current_time + ".html", "wb")
+
+		response = urllib.request.urlopen("http://coinmarketcap.com/?pages=" + str(page_num))
+		html = response.read()
+
+
+		f.write(html)
+		f.close()
+		time.sleep(60)
+	time.sleep(300)
